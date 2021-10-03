6541078b409f3726063ca38f72ebaf7ac3e80aab
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2021-10-03 17:02:16 -0300

    atualizando arquivos
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const path = require("path"); 
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded( { extended: true } ));
+
+let text = '';
+
+const pokedex = [
+    {
+    num: "012",
+    nome: "Butterfree",
+    tipo: "Bug",
+    foto: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
+    desc: "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+    alt: "1.1m",
+    peso: "32Kg",
+    cat: "Borboleta",
+    hab: "Compound Eyes",
+    },
+    {
+    num: "012",
+    nome: "Butterfree",
+    tipo: "Bug",
+    foto: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
+    desc: "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+    alt: "1.1m",
+    peso: "32Kg",
+    cat: "Borboleta",
+    hab: "Compound Eyes",
+    },
+    {
+    num: "012",
+    nome: "Butterfree",
+    tipo: "Bug",
+    foto: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
+    desc: "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+    alt: "1.1m",
+    peso: "32Kg",
+    cat: "Borboleta",
+    hab: "Compound Eyes",
+    }
+];
+
+app.get("/", function (req, res) {
+    res.render("index.ejs", { pokedex: pokedex });
+});
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
